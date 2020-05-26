8550574bba3ffbbefe6784b2ab78bfc84f5fabc3
ekimov <ark_ek@mail.ru>
2020-05-26 19:07:46 +0300

    Some fix 3

--- docker-build/entrypoint.sh
+++ docker-build/entrypoint.sh
@@ -3,13 +3,16 @@
 function main() {
   echo "enter in the main location"
 
+  ls -a
+
   echo ${INPUT_PASSWORD} | docker login --username oauth --password-stdin cr.yandex
 
   DOCKERNAME="${INPUT_NAME}"
   echo "${DOCKERNAME}"
+  echo "${GITHUB_REF}"
 
-  docker build -t ${DOCKERNAME} .
-  docker tag ${DOCKERNAME} ${INPUT_REGISTRY}/${DOCKERNAME}
+  docker build -t ${INPUT_REGISTRY}/${DOCKERNAME}:${GITHUB_REF} .
+  # docker tag ${DOCKERNAME} ${INPUT_REGISTRY}/${DOCKERNAME}
   docker push ${INPUT_REGISTRY}/${DOCKERNAME}
 
   docker logout
