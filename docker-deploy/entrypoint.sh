3bf27b38efa939a88945b7366cde199b792d400d
ekimov <ark_ek@mail.ru>
2020-05-27 15:45:15 +0300

    Some changes 15

--- docker-deploy/entrypoint.sh
+++ docker-deploy/entrypoint.sh
@@ -22,8 +22,8 @@ function main() {
   ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSH_PATH/dep_key -f -o ExitOnForwardFailure=yes -L 127.0.0.1:6789:/var/run/docker.sock $INPUT_USER@$INPUT_HOST sleep 10
   echo ${INPUT_PASSWORD} | docker -H 127.0.0.1:6789 login --username oauth --password-stdin cr.yandex
 
-  echo "${INPUT_REGISTRY}/${DOCKERNAME}:${GITHUB_SHA:0:8}"
-  
+  echo "${INPUT_REGISTRY}/site:${GITHUB_SHA:0:8}"
+
   cd ./docker-compose
   docker-compose up -d traefik
   docker-compose up -d site
