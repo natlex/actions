57b5bfb1ee4c8ca88e250101a9e349bc1648a0d4
ekimov <ark_ek@mail.ru>
2020-05-27 13:57:01 +0300

    Some changes 11

--- docker-deploy/entrypoint.sh
+++ docker-deploy/entrypoint.sh
@@ -19,8 +19,10 @@ function main() {
   ssh-add "$SSH_PATH/dep_key"
   ssh-keyscan -t rsa $INPUT_HOST >> "$SSH_PATH/known_hosts"
 
+  echo "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSH_PATH/dep_key -f -o ExitOnForwardFailure=yes -L 127.0.0.1:6789:/var/run/docker.sock $INPUT_USER@$INPUT_HOST sleep 10"
   ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSH_PATH/dep_key -f -o ExitOnForwardFailure=yes -L 127.0.0.1:6789:/var/run/docker.sock $INPUT_USER@$INPUT_HOST sleep 10
 
+  echo "${INPUT_PASSWORD} | docker -H 127.0.0.1:6789 login --username oauth --password-stdin cr.yandex"
   echo ${INPUT_PASSWORD} | docker -H 127.0.0.1:6789 login --username oauth --password-stdin cr.yandex
 
   cd ./docker-compose
