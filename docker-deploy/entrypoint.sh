0c3c6a75ee3d6a5519a5770dc2b35212c44ffa68
ekimov <ark_ek@mail.ru>
2020-05-28 11:27:55 +0300

    Some changes 19

--- docker-deploy/entrypoint.sh
+++ docker-deploy/entrypoint.sh
@@ -19,14 +19,13 @@ function main() {
   ssh-add "$SSH_PATH/dep_key"
   ssh-keyscan -t rsa $INPUT_HOST >> "$SSH_PATH/known_hosts"
 
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $SSH_PATH/dep_key -f -o ExitOnForwardFailure=yes -L 127.0.0.1:6789:/var/run/docker.sock $INPUT_USER@$INPUT_HOST sleep 10
+  ssh -o StrictHostKeyChecking=no -i $SSH_PATH/dep_key -f -o ExitOnForwardFailure=yes -L 127.0.0.1:6789:/var/run/docker.sock $INPUT_USER@$INPUT_HOST sleep 10
   echo ${INPUT_PASSWORD} | docker -H 127.0.0.1:6789 login --username oauth --password-stdin cr.yandex
 
   export IMAGE="${INPUT_REGISTRY}/site:${GITHUB_SHA:0:8}"
   echo "$IMAGE"
   cd ./docker-compose
-  DOCKER_HOST="127.0.0.1:6789" docker-compose up -d traefik
-  DOCKER_HOST="127.0.0.1:6789" docker-compose up -d site
+  DOCKER_HOST="127.0.0.1:6789" docker-compose up
 
   docker logout cr.yandex
 }
