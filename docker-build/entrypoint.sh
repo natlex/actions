5ec8cab40a33c8301aa3827922bffb39204d4b40
Andrew Nazarov <andrew.nazarov.intres@gmail.com>
2020-06-04 21:31:02 +0300

    Use Docker cache

--- docker-build/entrypoint.sh
+++ docker-build/entrypoint.sh
@@ -7,12 +7,17 @@ function main() {
 
   echo ${INPUT_PASSWORD} | docker login --username oauth --password-stdin cr.yandex
 
-  DOCKERNAME="${INPUT_NAME}"
-  echo "${DOCKERNAME}"
+  IMAGE_NAME="${INPUT_NAME}"
+  echo "${IMAGE_NAME}"
   echo "${GITHUB_SHA}"
 
-  docker build -t ${INPUT_REGISTRY}/${DOCKERNAME}:${GITHUB_SHA:0:8} .
-  docker push ${INPUT_REGISTRY}/${DOCKERNAME}:${GITHUB_SHA:0:8}
+  docker pull ${INPUT_REGISTRY}/${IMAGE_NAME}:latest || true
+
+  DOCKER_BUILDKIT=1 docker build --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ${INPUT_REGISTRY}/${IMAGE_NAME}:latest -t ${INPUT_REGISTRY}/${IMAGE_NAME}:${GITHUB_SHA:0:8} .
+  docker push ${INPUT_REGISTRY}/${IMAGE_NAME}:${GITHUB_SHA:0:8}
+
+  docker tag ${INPUT_REGISTRY}/${IMAGE_NAME}:${GITHUB_SHA:0:8} ${INPUT_REGISTRY}/${IMAGE_NAME}:latest
+  docker push ${INPUT_REGISTRY}/${IMAGE_NAME}:latest
 
   docker logout cr.yandex
 }
