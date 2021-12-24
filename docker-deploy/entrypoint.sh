89fcdfcb20ec980040f90463c2401aedd15155e5
Arkadiy Ekimov <ark_ek@mail.ru>
2021-12-24 11:33:23 +0300

    Add gcloud deploy action 5

--- docker-deploy/entrypoint.sh
+++ docker-deploy/entrypoint.sh
@@ -20,7 +20,7 @@ function main() {
 
   cd ./docker-compose
   ssh -o StrictHostKeyChecking=no -i $SSH_PATH/dep_key -f -o ExitOnForwardFailure=yes -L 127.0.0.1:6789:/var/run/docker.sock $INPUT_USER@$INPUT_HOST sleep 10
-  docker -H 127.0.0.1:6789 login --username ${INPUT_REGISTRY_USER} --password-stdin ${INPUT_REGISTRY_HOST}
+  echo ${INPUT_PASSWORD} | docker -H 127.0.0.1:6789 login --username ${INPUT_REGISTRY_USER} --password-stdin ${INPUT_REGISTRY_HOST}
   DOCKER_HOST="127.0.0.1:6789" docker-compose up -d
   docker -H 127.0.0.1:6789 logout ${INPUT_REGISTRY_HOST}
 }
